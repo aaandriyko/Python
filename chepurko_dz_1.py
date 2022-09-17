afe47fa1aceb1568e25259c32d251abb85778e64
Andrii Chepurko <chepurko2.andreyooops@gmail.com>
2022-09-17 20:21:33 +0300

    Homework #1

---
+++ chepurko_dz_1.py
@@ -0,0 +1,36 @@
+#Exercise 1:
+print("Hello World")
+
+#Exercise 2:
+print("Hello!\n", "I'm Andrii Chepurko\n", "I'm 23 y.o.")
+
+#Exercise 3:
+a = int(input("Enter length: "))
+b = int(input("Enter width: "))
+
+s = a * b
+
+print("The area of your rectangle is: ", s)
+
+#Exercise 4:
+num1 = int(input("Enter 1 number: "))
+num2 = int(input("Enter 2 number: "))
+
+sum1 = num1 + num2
+prod1 = num1 * num2
+diff1 = num1 - num2
+quot1 = num1 / num2
+
+print("Sum = ", sum1)
+print("Product = ", prod1)
+print("Difference = ", diff1)
+print("Quotient = ", quot1)
+
+#Exercise 5:
+r = float(input("Enter the raduis of a circle: "))
+
+PI = 3.14159
+
+print("Diameter: ", 2 * r)
+print("Circumference: ", 2 * PI * r)
+print("Area: ", PI * r ** 2)
