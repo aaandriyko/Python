4fb1cad0dc904c9e274d0428fc2e9256f4b30ace
Andrii Chepurko <113670437+aaandriyko@users.noreply.github.com>
2022-09-18 13:59:20 +0300

    Homework #3

--- chepurko_dz_3.py
+++ chepurko_dz_3.py
@@ -1,4 +1,4 @@
-#Exercise 1:
+#Exercise 1: Print the number entered by user only if it is negative
 a = int(input("Enter your number: "))
 b = min(a, 0)
 
@@ -7,7 +7,9 @@ if a:
 else:
     print("You entered 0")
 
-#Exercise 2:
+    
+    
+#Exercise 2: Check if the value is less than 20 or not
 c = int(input("Enter your number: "))
 d = 20 - c
 
@@ -17,17 +19,23 @@ else:
     print(max(0, d) or "Number is bigger than 20")
     print(max(0, d) and "Number is less than 20")
 
-#Exercise 3:
+    
+    
+#Exercise 3: Check if a given number 0 or not
 e = int(input("Enter your number: "))
 print(e and "Not 0")
 
-#Exercise 4:
+
+
+#Exercise 4: Check if a given number Even or Odd
 f = int(input("Enter your number: "))
 g = f % 2
 print(f % 2 or "Number is Even ")
 print(f % 2 and "Number is Odd")
 
-#Exercise 5:
+
+
+#Exercise 5: Find largest number among three given numbers entered by user
 num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 num3 = int(input("Enter 3rd number: "))
