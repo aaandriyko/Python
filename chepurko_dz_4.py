13900e17d679ae0b35d2ccfcc8d86d60a98a8dbd
Andrii Chepurko <113670437+aaandriyko@users.noreply.github.com>
2022-09-22 19:39:39 +0300

    Homework #4

--- chepurko_dz_4.py
+++ chepurko_dz_4.py
@@ -31,6 +31,29 @@ match y:
         else:
             print("Flat's order: ", number)
 
+            
+#Or
+#FLOORS = 9
+#FLATS_ON_FLOOR = 4
+#NUMBER_OF_ENTRANCES = 4
+#
+#flat = int(input('Flat number = '))
+#
+#if 0 < flat <= FLATS_ON_FLOOR * FLOORS * NUMBER_OF_ENTRANCES:
+#    
+#    entrance = (flat - 1) // (FLATS_ON_FLOOR * FLOORS) + 1
+#    floor = (flat - 1) // FLATS_ON_FLOOR - (entrance - 1) * FLOORS + 1
+#    flat_on_floor = (flat - 1) % 4 + 1
+#    
+#print(flat, entrance, floor, flat_on_floor)
+#
+#else:
+#    print('Wrong flat number')
+
+
+
+#----------------------------------------------------------------
+
 
 
 #Exercise 2: Leap year
@@ -40,6 +63,10 @@ if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print("A leap year")
 else:
     print("Not a leap year")
+
+    
+    
+#----------------------------------------------------------------
 
 
 
@@ -52,3 +79,11 @@ if (a + b) > c and (a + c) > b and (b + c) > a:
     print("This triangle exists")
 else:
     print("This triangle can't exist")
+    
+    
+#Or
+#a, b, c = int(input('a=')), int(input('b=')), int(input('c='))
+#Or
+#a, b, c = int(input('a=')),\
+           int(input('b=')),\
+           int(input('c='))
